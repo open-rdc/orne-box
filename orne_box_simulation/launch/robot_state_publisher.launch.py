cb6a745424756472f1224d3ee201b08146037437
haruyama8940 <yaruo0406@gmail.com>
2023-05-29 18:19:17 +0900

    add ros2 description ,setup , simulation

---
+++ orne_box_simulation/launch/robot_state_publisher.launch.py
@@ -0,0 +1,63 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+
+    packages_name = "orne_box_description"
+    xacro_file_name = "orne_box_3d_lidar.urdf.xacro"
+    rviz_file_name = "urdf.rviz"
+
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare(packages_name), "urdf", xacro_file_name]
+            ),
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare(packages_name), "rviz", rviz_file_name]
+    )
+
+    robot_state_pub_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+    
+    # joint_state_pub_gui_node = Node(
+    #     package="joint_state_publisher_gui",
+    #     executable="joint_state_publisher_gui",
+    #     output="screen",
+    # )
+    joint_state_pub_node = Node(
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+        output="screen",
+    )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+    )
+    
+    nodes = [
+        rviz_node,
+        robot_state_pub_node,
+        # joint_state_pub_gui_node
+        joint_state_pub_node
+    ]
+
+    return LaunchDescription(nodes)
---
+++ orne_box_simulation/launch/spawn_urdf.launch.py
@@ -0,0 +1,33 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    pkg_urdf_package = get_package_share_directory('orne_box_description')
+    urdf_file = os.path.join(pkg_urdf_package, 'urdf', 'orne_box.urdf.xacro')
+    
+    spawn_entity_node = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-entity', 'orne_box',
+                    '-topic', '/robot_description',
+                    '-x','0.0',
+                    '-y','0.0',
+                    '-z','0.0'
+                    ],
+
+        output='screen',
+    )
+
+    ld = LaunchDescription()
+
+    # ld.add_action(DeclareLaunchArgument(''))
+
+    ld.add_action(spawn_entity_node)
+
+    return ld
