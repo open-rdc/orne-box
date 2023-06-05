34f26bac64dcd3d78ca48613a6b614ebf334bc15
haruyama8940 <yaruo0406@gmail.com>
2023-06-05 16:06:23 +0900

    develop navaigation2 for orne_box

---
+++ orne_box_navigation_executor/launch/navigation2.launch.py
@@ -0,0 +1,70 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    # <path>
+    pkg_dir = get_package_share_directory('orne_box_navigation_executor')
+    nav2_launch_file_dir = os.path.join(pkg_dir, 'launch')
+    config_dir = os.path.join(pkg_dir, 'config')
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    map_dir = LaunchConfiguration(
+        'map',
+        default=os.path.join(
+            config_dir,
+            'maps',
+            'cit_3f_map.yaml'))
+
+    param_file_name = 'nav2_params.yaml'
+
+    param_dir = LaunchConfiguration(
+        'params',
+        default=os.path.join(
+            config_dir,
+            'param',
+            param_file_name))
+
+    rviz_config_dir = os.path.join(
+        config_dir,
+        'rviz',
+        'nav2_default_view2.rviz')
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'map',
+            default_value=map_dir,
+            description='Full path to map file to load'),
+
+        DeclareLaunchArgument(
+            'params',
+            default_value=param_dir,
+            description='Full path to param file to load'),
+
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([nav2_launch_file_dir, '/bringup_launch.py']),
+            launch_arguments={
+                'map': map_dir,
+                'use_sim_time': use_sim_time,
+                'params': param_dir}.items(),
+        ),
+
+        Node(
+            package='rviz2',
+            node_executable='rviz2',
+            node_name='rviz2',
+            arguments=['-d', rviz_config_dir],
+            parameters=[{'use_sim_time': use_sim_time}],
+            output='screen'),
+    ])
