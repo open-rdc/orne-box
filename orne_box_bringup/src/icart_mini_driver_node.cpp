32a6ce5f668a6f0dfa9bbebefffad6a8e4f34cd4
haruyama8940 <yaruo0406@gmail.com>
2023-08-10 16:09:48 +0900

    debug geometry_msgs import

--- orne_box_bringup/src/icart_mini_driver_node.cpp
+++ orne_box_bringup/src/icart_mini_driver_node.cpp
@@ -4,7 +4,7 @@
 #include "sensor_msgs/msg/joint_state.hpp"
 // #include <memory>
 #include <tf2/utils.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2_ros/transform_listener.h>
