a2fcafb5131c5d0246e505f76966e5146705d08f
Shuma-s58 <shuma.suzuki.2021@outlook.jp>
2026-05-29 01:17:49 +0000

    change map_file

--- orne_box_navigation_executor/launch/play_waypoints_nav.launch.py
+++ orne_box_navigation_executor/launch/play_waypoints_nav.launch.py
@@ -13,8 +13,8 @@ def generate_launch_description():
     launch_file_dir = os.path.join(get_package_share_directory('orne_box_navigation_executor'), 'launch')
     config_dir = os.path.join(nav_dir, 'config')
 
-    map_pass = 'tsudanuma'
-    #map_pass = 'cit_3f_map'
+    #map_pass = 'tsudanuma'
+    map_pass = 'cit_3f_map'
     bt_file_name ='navigate_w_replanning_and_wait.xml'
 
     map_data = LaunchConfiguration('map', default=os.path.join(config_dir, 'maps', map_pass + '.yaml'))
