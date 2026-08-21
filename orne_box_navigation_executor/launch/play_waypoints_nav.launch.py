0fd782221eb9becfe3afe90e89c29409ad7e3ea6
Shuma-s58 <shuma.suzuki.2021@outlook.jp>
2026-08-21 06:44:40 +0000

    add denoise_layer and inflation_layer

--- orne_box_navigation_executor/launch/play_waypoints_nav.launch.py
+++ orne_box_navigation_executor/launch/play_waypoints_nav.launch.py
@@ -14,7 +14,8 @@ def generate_launch_description():
     config_dir = os.path.join(nav_dir, 'config')
 
     #map_pass = 'tsudanuma'
-    map_pass = 'cit_3f_map'
+    map_pass = 'elevation_tsudanuma_2_round_map'
+    #map_pass = 'cit_3f_map'
     bt_file_name ='navigate_w_replanning_and_wait.xml'
 
     map_data = LaunchConfiguration('map', default=os.path.join(config_dir, 'maps', map_pass + '.yaml'))
