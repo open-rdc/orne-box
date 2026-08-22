2645434f7b0959d34d9cde0ee893a4da81440ed2
Shuma-s58 <shuma.suzuki.2021@outlook.jp>
2026-08-22 05:24:37 +0000

    add ground_consistency_layer in global_costmap

--- orne_box_navigation_executor/launch/play_waypoints_nav.launch.py
+++ orne_box_navigation_executor/launch/play_waypoints_nav.launch.py
@@ -15,7 +15,7 @@ def generate_launch_description():
 
     #map_pass = 'tsudanuma'
     map_pass = 'elevation_tsudanuma_2_round_map'
-    map_pass = 'cit_3f_map'
+    #map_pass = 'cit_3f_map'
     bt_file_name ='navigate_w_replanning_and_wait.xml'
 
     map_data = LaunchConfiguration('map', default=os.path.join(config_dir, 'maps', map_pass + '.yaml'))
