d75a89b7a28f9c015158a0d163c68df0e98f2a18
Shuma-s58 <shuma.suzuki.2021@outlook.jp>
2026-09-11 10:07:21 +0000

    teleop.launch.py

--- orne_box_bringup/launch/include/teleop.launch.py
+++ orne_box_bringup/launch/include/teleop.launch.py
@@ -10,11 +10,13 @@ def generate_launch_description():
     joy_config = launch.substitutions.LaunchConfiguration('joy_config')
     joy_dev = launch.substitutions.LaunchConfiguration('joy_dev')
     config_filepath = launch.substitutions.LaunchConfiguration('config_filepath')
+    use_sim_time = launch.substitutions.LaunchConfiguration('use_sim_time')
 
     return launch.LaunchDescription([
         launch.actions.DeclareLaunchArgument('joy_vel', default_value='cmd_vel'),
         launch.actions.DeclareLaunchArgument('joy_config', default_value='AIF_joy'),
         launch.actions.DeclareLaunchArgument('joy_dev', default_value='/dev/input/js0'),
+        launch.actions.DeclareLaunchArgument('use_sim_time', default_value='false'),
         launch.actions.DeclareLaunchArgument('config_filepath', default_value=[
             launch.substitutions.TextSubstitution(text=os.path.join(
                 get_package_share_directory('orne_box_bringup'), 'config/joy/', '')),
@@ -26,10 +28,12 @@ def generate_launch_description():
                 'dev': joy_dev,
                 'deadzone': 0.3,
                 'autorepeat_rate': 20.0,
+                'use_sim_time': use_sim_time,
             }]),
         launch_ros.actions.Node(
             package='teleop_twist_joy', executable='teleop_node',
-            name='teleop_twist_joy_node', parameters=[config_filepath],
+            name='teleop_twist_joy_node',
+            parameters=[config_filepath, {'use_sim_time': use_sim_time}],
             remappings={('/cmd_vel', launch.substitutions.LaunchConfiguration('joy_vel'))},
             ),
     ])
